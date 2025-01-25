d365b476b1ec47ebdc09d3e528a56c7572cfc339
Nick Baughman <baughmann1@gmail.com>
2025-01-24 22:36:51 -0500

    more examples

--- src/tikara/__init__.py
+++ src/tikara/__init__.py
@@ -1,3 +1,4 @@
 from tikara.tika import Tika
+from tikara.tika_util import TikaraUnpackedItem, LanguageConfidence, TikaraDetectLanguageResult
 
-__all__ = ["Tika"]
+__all__ = ["Tika", "TikaraUnpackedItem", "LanguageConfidence", "TikaraDetectLanguageResult"]
